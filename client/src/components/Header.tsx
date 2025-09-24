cbfdd0c708d585e9005bd2091b5510114b0717c2
pythagora <devuser@pythagora.ai>
2025-09-24 20:52:39 +0000

    new updates

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -75,10 +75,10 @@ export function Header() {
                 </div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>
+              {/* <DropdownMenuItem>
                 <User className="mr-2 h-4 w-4" />
                 <span>Profile</span>
-              </DropdownMenuItem>
+              </DropdownMenuItem> */}
               <DropdownMenuSeparator />
               <DropdownMenuItem onClick={handleLogout}>
                 <LogOut className="mr-2 h-4 w-4" />
